79abcf9a941ec39b20e11f2924ac895237ddfdc9
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-13 17:34:28 -0300

    index updated

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Module
+var resolution = new unResolution.Resolution();
+
+// Params
+const urlSearchParams = new URLSearchParams(window.location.search);
+var params = Object.fromEntries(urlSearchParams.entries());
