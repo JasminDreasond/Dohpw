d8a35fb7a8450479718efb61013d4e564fa1e5c2
Yasmin Seidel <jackiedevpe@outlook.com.br>
2022-01-13 18:05:40 -0300

    preparing first script

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ var resolution = new unResolution.Resolution();
 // Params
 const urlSearchParams = new URLSearchParams(window.location.search);
 var params = Object.fromEntries(urlSearchParams.entries());
+
+// Start App
+$(() => {
+
+    // Exist Domain
+    if (typeof params.d === 'string' && params.d.length > 0) {
+        $.LoadingOverlay("show", { background: "rgba(0,0,0, 0.5)", text: `Loading ${params.d}` });
+    }
+
+});
